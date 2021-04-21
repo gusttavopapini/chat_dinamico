15c4acd157f0bfd203708fb5dabd26db2df2594f
Gustavo Papini <gusttavopapini@gmail.com>
2021-04-21 15:31:35 -0300

    novo commit - criando ações

--- app/src/main/java/com/example/chatdinamico/MainActivity.kt
+++ app/src/main/java/com/example/chatdinamico/MainActivity.kt
@@ -1,19 +1,35 @@
 package com.example.chatdinamico
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         send_button.setOnClickListener {
-        val mensagem = edit_text.text.toString()
-       edit_text.setText("")
-       mensagem_text2.text = mensagem
+
+            var mensagemEnviada = edit_text.text.toString()
+
+            val envioDaMensagem = intent.putExtra("MENSAGEM_ENVIADA", mensagemEnviada)
+
+            val intent = Intent ( this@MainActivity, MainActivity2::class.java )
+
+            startActivity(intent)
+        }
+        qualTelaEstou.setOnClickListener {
+            Toast.makeText(this, "tela 1", Toast.LENGTH_LONG).show()
+        }
+
+
         }
     }
-}
--- app/src/main/java/com/example/chatdinamico/MainActivity2.kt
+++ app/src/main/java/com/example/chatdinamico/MainActivity2.kt
@@ -1,4 +1,16 @@
 package com.example.chatdinamico
 
-class MainActivity2 {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        var intent = intent
+        val mensagemRecebida = intent.getStringExtra("MENSAGEM_ENVIADA")
+        mensagem_text2.setText(mensagemRecebida)
+    }
 }
