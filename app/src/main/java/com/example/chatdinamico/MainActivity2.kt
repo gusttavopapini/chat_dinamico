a051b68d95e2cddcd3547d9d742fee94266265eb
Gustavo Papini <gusttavopapini@gmail.com>
2021-04-21 15:49:14 -0300

    botão tela 2 - "qual tela estou?"

--- app/src/main/java/com/example/chatdinamico/MainActivity2.kt
+++ app/src/main/java/com/example/chatdinamico/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.chatdinamico
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main2.*
 
@@ -12,5 +13,8 @@ class MainActivity2 : AppCompatActivity() {
         var intent = intent
         val mensagemRecebida = intent.getStringExtra("MENSAGEM_ENVIADA")
         mensagem_text2.setText(mensagemRecebida)
+
+        qualTelaEstou2.setOnClickListener {
+            Toast.makeText(this, "tela 2", Toast.LENGTH_LONG).show()
     }
 }
