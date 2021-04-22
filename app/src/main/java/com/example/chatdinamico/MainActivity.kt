9ddce227a86142426beee8a841663674db60dd90
Gustavo Papini <gusttavopapini@gmail.com>
2021-04-22 01:37:07 -0300

    Organizando o código

--- app/src/main/java/com/example/chatdinamico/MainActivity.kt
+++ app/src/main/java/com/example/chatdinamico/MainActivity.kt
@@ -3,43 +3,30 @@ package com.example.chatdinamico
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         //ENVIO DA MENSAGEM DA TELA 1
         send_button.setOnClickListener {
-
            var mensagemEnviada = edit_text.text.toString()
-
             val intent = Intent (this@MainActivity, MainActivity2::class.java)
             intent.putExtra("MENSAGEM_ENVIADA", mensagemEnviada)
-
             startActivity(intent)
         }
 
-
         //RECEBIMENTO DA MENSAGEM DA TELA 2
         val mensagemRecebida = intent.getStringExtra("MENSAGEM_ENVIADA")
-
         mensagem_text.text = mensagemRecebida
-
-
 
         //QUAL TELA ESTOU?
         qualTelaEstou.setOnClickListener {
             Toast.makeText(this, "tela 1", Toast.LENGTH_LONG).show()
         }
-
-        }
     }
+}
--- app/src/main/java/com/example/chatdinamico/MainActivity2.kt
+++ app/src/main/java/com/example/chatdinamico/MainActivity2.kt
@@ -2,7 +2,6 @@ package com.example.chatdinamico
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,28 +12,21 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-
         //ENVIO DA MENSAGEM DA TELA 1
         send_button2.setOnClickListener {
-
             var mensagemEnviada = edit_text2.text.toString()
-
             val intent = Intent (this@MainActivity2, MainActivity::class.java)
             intent.putExtra("MENSAGEM_ENVIADA", mensagemEnviada)
-
             startActivity(intent)
         }
 
-
-//RECEBIMENTO DA MENSAGEM DA TELA 1
+        //RECEBIMENTO DA MENSAGEM DA TELA 1
         val mensagemRecebida = intent.getStringExtra("MENSAGEM_ENVIADA")
-
         mensagem_text2.text = mensagemRecebida
-
 
         //QUAL TELA ESTOU?
         qualTelaEstou2.setOnClickListener {
             Toast.makeText(this, "tela 2", Toast.LENGTH_LONG).show()
+        }
     }
-}
 }
